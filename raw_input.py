20990032cc824b54fab0d250a2181081732d82e8
= <=>
2024-02-25 14:59:22 +0000

    collected posix and nt function in separate if blocks

--- raw_input.py
+++ raw_input.py
@@ -1,43 +1,46 @@
 """ cross platofrm module to handle raw input from terminal """
 import os
 import sys
+from typing import NoReturn
 
+# posix utils
 if os.name == "posix":
     import termios
     import tty
-elif os.name == "nt":
-    from msvcrt import getch as getch_encoded
-else:
-    sys.exit("Operating system not recognised")
 
+    class Vars:
+        """Posix terminal variables"""
 
-# CLEAN TERMINAL
-if os.name == "posix":
-    def clear():
-        """ clear screen """
-        os.system("clear")
-elif os.name == "nt":
-    def clear():
-        """ clear screen """
-        os.system("cls")
-
-
-# FETCH KEYBOARD INPUT
-if os.name == "posix":
-    def getch() -> str:
-        """ read raw terminal input """
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
+        conv_arrows = {"D": "left", "C": "right", "A": "up", "B": "down"}
+
+    VARS = Vars()
+
+    def keyboard_attach() -> NoReturn:
+        """attach keyboard input"""
+        tty.setraw(VARS.fd)
+
+    def keyboard_detach() -> NoReturn:
+        """detach keyboard input"""
+        termios.tcsetattr(VARS.fd, termios.TCSADRAIN, VARS.old_settings)
+
+    def clear() -> NoReturn:
+        """clear screen"""
+        os.system("clear")
+
+    def getch() -> str:
+        """read raw terminal input"""
+
         try:
-            tty.setraw(sys.stdin.fileno())
+            keyboard_attach()
             ch = sys.stdin.read(1)
         finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            keyboard_detach()
         return ch
 
-    conv_arrows = {"D": "left", "C": "right", "A": "up", "B": "down"}
-    def get_key() -> str:
-        """ process correct string for keyboard input """
+    def getkey() -> str:
+        """process correct string for keyboard input"""
         key_pressed = getch()
         match key_pressed:
             case "\r":
@@ -46,24 +49,36 @@ if os.name == "posix":
                 return "tab"
             case "\x1b":
                 if getch() == "[":
-                    return conv_arrows.get(getch(), None)
+                    return VARS.conv_arrows.get(getch(), None)
             case "\x7f":
                 return "backspace"
             case _:
                 return key_pressed
 
 elif os.name == "nt":
+    from msvcrt import getch as getch_encoded
+
+    class Vars:
+        """Windows terminal variables"""
+
+        conv_arrows = {"K": "left", "M": "right", "H": "up", "P": "down"}
+
+    VARS = Vars()
+
+    def clear() -> NoReturn:
+        """clear screen"""
+        os.system("cls")
+
     def getch() -> str:
-        """ read raw terminal input """
+        """read raw terminal input"""
         letter = getch_encoded()
         try:
-            return letter.decode('ascii')
-        except:
+            return letter.decode("ascii")
+        except SyntaxError:
             return letter
 
-    conv_arrows = {"K": "left", "M": "right", "H": "up", "P": "down"}
-    def get_key() -> str:
-        """ process correct string for keyboard input """
+    def getkey() -> str:
+        """process correct string for keyboard input"""
         key_pressed = getch()
         match key_pressed:
             case "\r":
@@ -71,8 +86,11 @@ elif os.name == "nt":
             case "\t":
                 return "tab"
             case b"\xe0":
-                return conv_arrows.get(getch(), None)
+                return VARS.conv_arrows.get(getch(), None)
             case b"\x08":
                 return "backspace"
             case _:
                 return key_pressed
+
+else:
+    sys.exit("Operating system not recognised")
